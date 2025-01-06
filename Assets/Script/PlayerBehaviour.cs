b950bf82c68baf5061459147d5a6535989acb87f
thien2001git <hoangthien66771508@gmail.com>
2025-01-06 21:49:21 +0700

    update PlayerBehaviour.cs

--- Assets/Script/PlayerBehaviour.cs
+++ Assets/Script/PlayerBehaviour.cs
@@ -5,23 +5,51 @@ using UnityEngine;
 public class PlayerBehaviour : MonoBehaviour
 {
     public float moveSpeed = 5f;
-    public float rotationSpeed = 720f;
-   
 
     Vector2 movement;
 
     public Animator animator;
     public Rigidbody2D rb;
-  
+
     void Update()
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
 
-        animator.SetFloat("Horizontal", movement.x);
-        animator.SetFloat("Vertical", movement.y);
-        animator.SetFloat("Speed", movement.sqrMagnitude);
+        DirectionUpdate();
+
+        bool isWalking = movement.x != 0 || movement.y != 0;
+        bool isAttack = Input.GetMouseButtonDown(0);
+
+        if (isWalking)
+        {
+            animator.SetFloat("Horizontal", movement.x);
+            animator.SetFloat("Vertical", movement.y);
+        }
+
+        animator.SetBool("IsWalking", isWalking);
+        animator.SetBool("IsAttack", isAttack);
         FixedUpdate();
+    }
+
+    void DirectionUpdate()
+    {
+        if (movement.x == -1)
+        {
+            animator.SetFloat("Direction", 4);
+        }
+        else if (movement.x == 1)
+        {
+            animator.SetFloat("Direction", 2);
+        }
+        else if (movement.y == -1)
+        {
+            animator.SetFloat("Direction", 1);
+        }
+        else if (movement.y == 1)
+        {
+            animator.SetFloat("Direction", 3);
+        }
     }
 
     private void FixedUpdate()
